b83f6ebcd8b9c0de78bbdce0785237e7692a11fc
nbr233 <nurunnabireachad233@gmail.com>
2025-08-31 02:04:01 +0600

    sum of 2 number

---
+++ sum_2number.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+void sum_2number() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Sum: " << a + b << endl;
+}
+int main() {
+    sum_2number();
+    return 0;
+}
